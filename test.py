801324e83e1e516c63a5601d7e574c6c838a30f7
geoffrey-sr <geoffreyyuen@stellarisrobot.com>
2026-03-12 11:42:18 +0800

    testing broken file

--- test.py
+++ test.py
@@ -1,17 +1,18 @@
 # broken_test.py
 
+import os, sys  # noqa: E402 to prevent removal by autoformatter
+
+print(os.name)  # use os somewhere
+foo = sys.version  # use sys somewhere
 
 def foo(a, b):
     print(a + b)  # whitespace around arguments (ruff: E203)
 
-
 def bar():
-    x = 1  # wrong indentation (ruff: E111)
+    x = 1
     y = 2
-    return x + y  # missing whitespace (ruff: E231)
-
+    return x + y
 
 foo(1, 2)
-bar()  # semicolon usage (ruff: E703)
-
+bar()
 unused_var = 123  # unused variable (ruff: F841)
