ae71c247bb095a343883ebb90d6c04bd7910002f
geoffrey-sr <geoffreyyuen@stellarisrobot.com>
2026-03-12 11:53:23 +0800

    modify test

--- test.py
+++ test.py
@@ -1,17 +1,19 @@
 # broken_test.py
 
-import os, sys  # noqa: E402 to prevent removal by autoformatter
+import os
 
 print(os.name)  # use os somewhere
-foo = sys.version  # use sys somewhere
+
 
 def foo(a, b):
     print(a + b)  # whitespace around arguments (ruff: E203)
+
 
 def bar():
     x = 1
     y = 2
     return x + y
+
 
 foo(1, 2)
 bar()
